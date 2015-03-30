52acdf2346cd60ac31185971515960f43cde5d3e
KemCake <santos.remi@icloud.com>
2015-03-29 17:58:55 -0700

    Initial Commit

---
+++ RemiSantos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemiSantos
+//
+//  Created by Remi Santos on 29/03/15.
+//  Copyright (c) 2015 Remi Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
